9ec514cbe3c9d6f883ce75da7f0e5ad84efefa41
hasan <36068229+heidaridk@users.noreply.github.com>
2019-04-29 16:07:25 +0200

    extra sout til føjet for ekstra tjek

--- src/main/java/helloworld/Hello.java
+++ src/main/java/helloworld/Hello.java
@@ -9,6 +9,8 @@ public class Hello {
     @GetMapping("/")
     public String home(){
         System.out.println("index called");
+        System.out.println("index is done ");
+        System.out.println("index is running ");
         return "index";
     }
 }
