d7e8de7a5267dee2d3ea159a5dcbdcb30bc0bdd8
hasan <36068229+heidaridk@users.noreply.github.com>
2019-04-29 14:38:11 +0200

    Initial commit

---
+++ src/main/java/helloworld/Hello.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Hello {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
---
+++ src/main/java/helloworld/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package helloworld.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
